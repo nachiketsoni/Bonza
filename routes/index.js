2ac09504f8239b17a6bf55f2e35fbcfe546aeaa1
nachiketsoni <98284413+nachiketsoni@users.noreply.github.com>
2022-08-15 17:22:45 +0530

    landing page done

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/product', function(req, res, next) {
+  res.render('product');
+});
 
 module.exports = router;
